2241d7af3dad9add6e387f558e56c437016f6e52
Samir Kahvedzic <skahvedzic@capeannenterprises.com>
2022-02-23 22:41:52 +0100

    Roles added

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+     /**
+     * App\Role relation.
+     *
+     * @return Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class)->withTimestamps();
+    }
 }
