c7e30f22f311d69d95fa1cb7e9ca9ea43bb36128
skahvedzic <samirkafa@gmail.com>
2022-03-08 07:38:37 +0100

    Added Donation, Donor and User

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace Database\Factories;
-
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -17,12 +16,14 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+       
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'author' => 'Samir',
         ];
     }
 
@@ -39,4 +40,5 @@ class UserFactory extends Factory
             ];
         });
     }
+
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use \App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +14,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+       
+       //create user
+        $userCount = (int)$this->command->ask('How many users would you like?', 20);
+       \App\Models\User::factory($userCount)->create();
+
+        //create donors
+        $donorCount = (int)$this->command->ask('How many donors would you like?', 20);
+        $userWithNoRelationship = \App\Models\User::doesnthave('roles')->get();
+        $userWithNoRelationship->each(function (User $user) {
+            $user->roles()->attach(3);
+            $user->save();
+
+        });
+
+       //create donations
+        $donationCount = (int)$this->command->ask('How many donations would you like?', 20);
+        $donors = \App\Models\User::whereHas('roles', function($query) {$query->where('name','donor');})->get();
+        \App\Models\Donation::factory($donationCount)->make()->each(function($donation) use ($donors){
+            $donation->user_id = $donors->random()->id;
+            $donation->save();
+        });
+
+
     }
 }
